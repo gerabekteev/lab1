b3236f3be2d775233a365e1ba244a272949e4fba
gerabekteev <bekteev.gera@yandex.ru>
2024-11-27 11:34:07 +0300

    основа есть короче, круто

--- js.py
+++ js.py
@@ -30,17 +30,17 @@ def add_preferential(data, preferential):
     data['preferential'].append(preferential.to_dict())
 
 
-def delete_one_time(data, title):
-    data['one_time'] = [temp for temp in data['one_time'] if temp['title'] != title]
+def delete_one_time(data, name_owner):
+    data['one_time'] = [temp for temp in data['one_time'] if temp['name_owner'] != name_owner]
 
 
-def delete_many_time(data, title):
-    data['many_time'] = [temp for temp in data['many_time'] if temp['title'] != title]
+def delete_many_time(data, name_owner):
+    data['many_time'] = [temp for temp in data['many_time'] if temp['name_owner'] != name_owner]
 
 
-def delete_subscription(data, title):
-    data['subscription'] = [temp for temp in data['subscription'] if temp['title'] != title]
+def delete_subscription(data, name_owner):
+    data['subscription'] = [temp for temp in data['subscription'] if temp['name_owner'] != name_owner]
 
 
-def delete_preferential(data, title):
-    data['preferential'] = [temp for temp in data['preferential'] if temp['title'] != title]
+def delete_preferential(data, name_owner):
+    data['preferential'] = [temp for temp in data['preferential'] if temp['name_owner'] != name_owner]
--- main.py
+++ main.py
@@ -45,15 +45,17 @@ def main():
 
     print("json / xml -")
     file_format = input().lower()
+    filename_json = 'data.json'
+    filename_xml = 'data.xml'
     if file_format == 'json':
         filename = 'data.json'
         data = js.load_from_json(filename)
         handler = js
-        # elif file_format == 'xml':
-        #     filename = 'data.xml'
-        #     data = xm.load_from_xml(filename)
-        #     handler = xm
-        # else:
+    elif file_format == 'xml':
+        filename = 'data.xml'
+        data = xm.load_from_xml(filename)
+        handler = xm
+    else:
         print("Неверный формат!")
         return
 
@@ -61,18 +63,17 @@ def main():
 
     while True:
 
-        print("\nВыберите действие:")
+        print("Выберите действие:")
         print("1 - Добавить одноразовый билет")
         print("2 - Добавить многоразовый билет")
         print("3 - Добавить подписочный билет")
         print("4 - Добавить льготный билет")
-        print("5 - Удалить фильм")
-        print("6 - Удалить сериал")
-        print("7 - Удалить сериал")
-        print("8 - Удалить сериал")
+        print("5 - Удалить одноразовый билет")
+        print("6 - Удалить  многоразовый билет")
+        print("7 - Удалить подписочный билет")
+        print("8 - Удалить льготный билет")
         print("10 - Сохранить")
         print("11 - Выход")
-        print("12 - Сравнить и синхронизировать JSON и XML")
         print("13 - Статистика по БД")
         print("14 - Вывести данные из JSON")
         print("15 - Вывести данные из XML")
@@ -93,72 +94,93 @@ def main():
             handler.add_one_time(data, exp)
 
         elif action == '2':
-            title = input("Введите название сериала: ")
-            director = input("Введите режиссера сериала: ")
-            year = get_year("Введите год выпуска сериала: ")
-            seasons = get_positive_int("Введите количество сезонов: ")
-            episodes = get_positive_int("Введите количество эпизодов: ")
-            tvseries = TVSeries(title, director, year, seasons, episodes)
-            handler.add_tvseries(data, tvseries)
+            name_owner = input("Введите имя владельца: ")
+            transport = input("Введите тип транспорта: ")
+            cl = input("Введите класс билета: ")
+            time_ti_buy = is_date_in_range("Введите дату оформления: ", start_date, end_date)
+            price = positiv_number("Введите цену: ")
+            col_use = positiv_number("введите количество использований:")
+            exp = many_time(name_owner, transport, cl, time_ti_buy,price,col_use)
+            handler.add_many_time(data, exp)
 
         elif action == '3':
-            title = input("Введите название фильма для удаления: ")
-            handler.delete_movie(data, title)
+            name_owner = input("Введите имя владельца: ")
+            transport = input("Введите тип транспорта: ")
+            cl = input("Введите класс билета: ")
+            time_ti_buy = is_date_in_range("Введите дату оформления: ", start_date, end_date)
+            price = positiv_number("Введите цену: ")
+            col_day = positiv_number("введите количество дней:")
+            exp = many_time(name_owner, transport, cl, time_ti_buy,price,col_day)
+            handler.add_subscription(data, exp)
 
         elif action == '4':
-            title = input("Введите название сериала для удаления: ")
-            handler.delete_tvseries(data, title)
+            name_owner = input("Введите имя владельца: ")
+            transport = input("Введите тип транспорта: ")
+            cl = input("Введите класс билета: ")
+            time_ti_buy = is_date_in_range("Введите дату оформления: ", start_date, end_date)
+            #price = positiv_number("Введите цену: ")
+            reason = input("введите причину выдачи")
+            exp = many_time(name_owner, transport, cl, time_ti_buy,0,reason)
+            handler.add_preferential(data, exp)
 
         elif action == '5':
-            json_.save_to_json(data, filename_json)
-            xml_.save_to_xml(data, filename_xml)
+            title = input("Введите имя владельца для удаления: ")
+            handler.delete_one_time(data, title)
+        elif action == '6':
+            title = input("Введите имя владельца для удаления: ")
+            handler.delete_many_time(data, title)
+        elif action == '7':
+            title = input("Введите имя владельца для удаления: ")
+            handler.delete_subscription(data, title)
+        elif action == '8':
+            title = input("Введите имя владельца для удаления: ")
+            handler.delete_preferential(data, title)
+
+        elif action == '10':
+            js.save_to_json(data, filename_json)
+            xm.save_to_xml(data, filename_xml)
             print(f"Данные сохранены в {filename_json} и {filename_xml}")
 
-        elif action == '6':
+        elif action == '11':
             break
 
-        elif action == '7':
-            json_data = json_.load_from_json('data.json')
-            xml_data = xml_.load_from_xml('data.xml')
-            sync_data(json_data, xml_data)
-            json_.save_to_json(json_data, 'data.json')
-            xml_.save_to_xml(xml_data, 'data.xml')
-            print("Данные успешно синхронизированы и сохранены в оба файла.")
-        elif action == '8':
-            show_statistics(data)
-        elif action == '999':
-            PIZDEC(data)
+        # elif action == '12':
+        #     json_data = js.load_from_json('data.json')
+        #     xml_data = xm.load_from_xml('data.xml')
+        #     sync_data(json_data, xml_data)
+        #     js.save_to_json(json_data, 'data.json')
+        #     xm.save_to_xml(xml_data, 'data.xml')
+        #     print("Данные успешно синхронизированы и сохранены в оба файла.")
         elif action == '13':
-            try:
-                if file_format != 'json':
+            print("один -", len(data["one_time"]))
+            print("много -",len(data["many_time"]))
+            print("подписка -",len(data["subscription"]))
+            print("льгота -",len(data["preferential"]))
+        # elif action == '999':
+        #     PIZDEC(data)
+        elif action == '14':
+            print("одноразовый:")
+            for exp in data['one_time']:
+                print(f"имя: {exp['name_owner']}, транспорт: {exp['transport']}, "
+                      f"класс: {exp['cl']}, время покупки: {exp['time_ti_buy']},"
+                      f"цена: {exp['price']}")
 
-                    raise InvalidFileFormatError("Неверный формат! Вы выбрали XML, а пытаетесь открыть JSON.")
-                else:
-                    filename = 'data.json'
-                    data = json_.load_from_json(filename)
-                    handler = json_
-                    print_data(data, file_format)
-            except InvalidFileFormatError as e:
-                print(f"Ошибка: {e}")
+            print("многоразовый:")
+            for exp in data['one_time']:
+                print(f"имя: {exp['name_owner']}, транспорт: {exp['transport']}, "
+                      f"класс: {exp['cl']}, время покупки: {exp['time_ti_buy']},"
+                      f"цена: {exp['price']},количество использований: {exp['col_use']}")
+            print("подписочный:")
+            for exp in data['one_time']:
+                print(f"имя: {exp['name_owner']}, транспорт: {exp['transport']}, "
+                      f"класс: {exp['cl']}, время покупки: {exp['time_ti_buy']},"
+                      f"цена: {exp['price']},количество дней: {exp['col_day']}")
+            print("льготный:")
+            for exp in data['one_time']:
+                print(f"имя: {exp['name_owner']}, транспорт: {exp['transport']}, "
+                      f"класс: {exp['cl']}, время покупки: {exp['time_ti_buy']},"
+                      f"цена: {exp['price']},причина: {exp['reason']}")
 
-        elif action == '169':
-            try:
-                if file_format != 'xml':
-                    counter += 1
-                    if counter == 1:
-                        raise InvalidFileFormatError("Бывает, промахнулся, ничего страшного.")
-                    elif counter == 2:
-                        raise InvalidFileFormatError("Чумба, попей колесики.")
-                    else:
-                        raise InvalidFileFormatError(
-                            "Ты меня пугаешь...неужели ты не понял что вообще нет смысла в разделении... и ничего мне не мешало немного изменить код? Я просто хотел хоть где то оставить свою лепту, ибо меняя названия переменных я бы потом вообще не понял, а где что....")
-                else:
-                    filename = 'data.xml'
-                    data = xml_.load_from_xml(filename)
-                    handler = xml_
-                    print_data(data, file_format)
-            except InvalidFileFormatError as e:
-                print(f"{e}")
 
         else:
             print("Неверная команда!")
